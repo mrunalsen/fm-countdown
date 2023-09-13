5df2a890324a078187a6111272fc93fca92888d4
mrunalsen <mrunalpatel2500@gmail.com>
2023-09-13 19:12:18 +0530

    feat: few animation tweaks

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -13,9 +13,9 @@ const Counter = () => {
         const timer = setInterval(() => {
             if (seconds > 0) {
                 setSeconds(seconds - 1);
-                setIsTimeUp(true);
             } else {
                 clearInterval(timer);
+                setIsTimeUp(true);
             }
         }, 1000);
 
@@ -23,8 +23,10 @@ const Counter = () => {
             clearInterval(timer);
         };
     }, [seconds]);
-
-    const timerClass = isTimeUp ? 'top-flip' : '';
+    // Format a number with a leading zero if it's a single digit
+    const formatNumber = (number) => {
+        return number < 10 ? `0${number}` : `${number}`;
+    };
     // Converting seconds into Days
     const days = Math.floor(seconds / 86400);
     // Converting seconds into Hours
@@ -37,38 +39,82 @@ const Counter = () => {
     return (
         <div className='flex mt-24'>
             <div className='flex flex-col items-center mx-4 relative'>
-                <div className={`absolute bg-timer1 ${hours === 0 ? 'top-flip' : ''}`}></div>
-                <div className="bg-timer relative rounded-md mx-2">
+                <div className="bg-timer relative rounded-md mx-2  ">
                     <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
-                    <p className={`text-rose-400 text-8xl p-4 ${hours === 0 ? 'top-flip' : ''}`}>{days}</p>
+                    <span className={`text-rose-400 text-8xl p-4 ${hours === 0 ? '' : ''}`}>{formatNumber(days)}</span>
                 </div>
-                <p className='text-gray-500 tracking-[5px] text-xs mt-4'>DAYS</p>
+                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>DAYS</span>
             </div>
             <div className='flex flex-col items-center mx-4 relative'>
-                <div className={`absolute bg-timer1 ${minutes === 0 ? 'top-flip' : ''}`}></div>
-                <div className="bg-timer relative rounded-md mx-2">
+                <div className="bg-timer relative rounded-md mx-2  ">
                     <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
-                    <p className={`text-rose-400 text-8xl p-4 ${minutes === 0 ? 'top-flip' : ''}`}>{hours}</p>
+                    <span className={`text-rose-400 text-8xl p-4 ${minutes === 0 ? '' : ''}`}>{formatNumber(hours)}</span>
                 </div>
-                <p className='text-gray-500 tracking-[5px] text-xs mt-4'>HOURS</p>
+                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>HOURS</span>
             </div>
             <div className='flex flex-col items-center mx-4 relative'>
-                <div className={`absolute bg-timer1 ${remainingSeconds === 0 ? 'top-flip' : ''}`}></div>
-                <div className={`bg-timer relative rounded-md mx-2`}>
-                    <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
-                    <p className={`text-rose-400 text-8xl p-4 ${remainingSeconds === 0 ? 'top-flip' : ''}`}>{minutes}</p>
+                <div className={`bg-timer relative rounded-md mx-2  `}>
+                    <div className={`flip-container ${remainingSeconds === 0 ? 'drop' : ''}`}>
+                        <span className={`text-rose-400 absolute text-8xl${remainingSeconds === 0 ? 'drop' : ''}`}>{formatNumber(minutes)}</span>
+                    </div>
+                    <span className={`text-rose-400 text-8xl p-4 ${remainingSeconds === 0 ? '' : ''}`}>{formatNumber(minutes)}</span>
                 </div>
-                <p className='text-gray-500 tracking-[5px] text-xs mt-4'>MINUTES</p>
+                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>MINUTES</span>
             </div>
             <div className='flex flex-col items-center mx-4 relative'>
-                <div className={`absolute bg-timer1 top-flip`}></div>
-                <div className="bg-timer relative rounded-md mx-2">
-                    <div className="absolute circle top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
-                    <p className='text-rose-400 text-8xl top-flip'>{remainingSeconds}</p>
+                <div className="bg-timer relative overflow-hidden rounded-md mx-2  ">
+                    <div className={`flip-container ${remainingSeconds === 0 ? 'drop' : ''}`}>
+                        <span className={`text-rose-400 absolute text-8xl ${remainingSeconds === 0 ? 'drop' : ''}`}>{formatNumber(minutes)}</span>
+                    </div>
+                    <span className={`text-rose-400 text-8xl${remainingSeconds === 0 ? 'fade' : ''}`}>{formatNumber(minutes)}</span>
                 </div>
-                <p className='text-gray-500 tracking-[5px] text-xs mt-4'>SECONDS</p>
+                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>SECONDS</span>
+            </div>
+            <div className='flex flex-col items-center mx-4 relative'>
+                <div className="bg-timer relative flex justify-center items-center overflow-hidden rounded-md mx-2  ">
+                    <div className="flip-container drop">
+                        <span className='text-rose-400 absolute text-8xl'>{formatNumber(remainingSeconds)}</span>
+                    </div>
+                    <span className='text-rose-400 absolute text-8xl'>{formatNumber(remainingSeconds)}</span>
+                    <span className='text-rose-400 fade text-8xl'>{formatNumber(remainingSeconds)}</span>
+                </div>
+                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>SECONDS</span>
             </div>
         </div>
+        // <div className='flex mt-24'>
+        //     <div className='flex flex-col items-center mx-4 relative'>
+        //         <div className={`absolute bg-timer1 ${hours === 0 ? '' : ''}`}></div>
+        //         <div className="bg-timer relative rounded-md mx-2  ">
+        //             <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
+        //             <p className={`text-rose-400 text-8xl p-4 ${hours === 0 ? '' : ''}`}>{formatNumber(days)}</p>
+        //         </div>
+        //         <p className='text-gray-500 tracking-[5px] text-xs mt-10'>DAYS</p>
+        //     </div>
+        //     <div className='flex flex-col items-center mx-4 relative'>
+        //         <div className={`absolute bg-timer1 ${minutes === 0 ? '' : ''}`}></div>
+        //         <div className="bg-timer relative rounded-md mx-2  ">
+        //             <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
+        //             <p className={`text-rose-400 text-8xl p-4 ${minutes === 0 ? '' : ''}`}>{formatNumber(hours)}</p>
+        //         </div>
+        //         <p className='text-gray-500 tracking-[5px] text-xs mt-10'>HOURS</p>
+        //     </div>
+        //     <div className='flex flex-col items-center mx-4 relative'>
+        //         <div className={`absolute bg-timer1 ${remainingSeconds === 0 ? '' : ''}`}></div>
+        //         <div className={`bg-timer relative rounded-md mx-2  `}>
+        //             <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
+        //             <p className={`text-rose-400 text-8xl p-4 ${remainingSeconds === 0 ? '' : ''}`}>{formatNumber(minutes)}</p>
+        //         </div>
+        //         <p className='text-gray-500 tracking-[5px] text-xs mt-10'>MINUTES</p>
+        //     </div>
+        //     <div className='flex flex-col items-center mx-4 relative'>
+        //         <div className={`absolute bg-timer1 `}></div>
+        //         <div className="bg-timer relative rounded-md mx-2  ">
+        //             <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
+        //             <p className='text-rose-400 text-8xl '>{formatNumber(remainingSeconds)}</p>
+        //         </div>
+        //         <p className='text-gray-500 tracking-[5px] text-xs mt-10'>SECONDS</p>
+        //     </div>
+        // </div>
     );
 };
 
--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const Socials = () => {
+    const socials = ['facebook', 'pinterest', 'instagram'];
+
     return (
         <div className='flex mt-auto mb-12'>
-            <p className='icon-facebook text-gray-500 hover:text-rose-400 text-3xl cursor-pointer p-2 mx-2'></p>
-            <p className='icon-pinterest text-gray-500 hover:text-rose-400 text-3xl cursor-pointer p-2 mx-2'></p>
-            <p className='icon-instagram text-gray-500 hover:text-rose-400 text-3xl cursor-pointer p-2 mx-2'></p>
+            {socials.map((media, index) => (
+                <p key={index} className={`icon-${media} text-gray-500 hover:text-rose-400 text-3xl cursor-pointer p-2 mx-2`}></p>
+            ))}
         </div>
     );
 };

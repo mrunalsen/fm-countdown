261355726f3bad9cdb52ccc69deca92a42cd336c
mrunalsen <mrunalpatel2500@gmail.com>
2023-09-20 13:03:47 +0530

    feat: mobile responsive

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,90 +3,108 @@ import React, { useEffect, useState } from 'react';
 const Counter = () => {
     // Number of days wanted for countdown
     const dayNum = 8;
+    // Initial seconds of 8 days
     const initialSeconds = dayNum * 24 * 60 * 60;
     const [seconds, setSeconds] = useState(initialSeconds);
     const remainingSeconds = seconds % 60;
-    const [animationClass, setAnimationClass] = useState('drop')
-    const [animationClass1, setAnimationClass1] = useState('below')
-    const [resetAnimation, setResetAnimation] = useState(false)
 
-    // Initial seconds of 8 days
-        // Format a number with a leading zero if it's a single digit
-        const formatNumber = (number) => {
-            return number < 10 ? `0${number}` : `${number}`;
-        };
-        // Converting seconds into Days
-        const days = Math.floor(seconds / 86400);
-        // Converting seconds into Hours
-        const hours = Math.floor((seconds % 86400) / 3600);
-        // Converting seconds into Minutes
-        const minutes = Math.floor((seconds % 3600) / 60);
-        // Converting seconds into Seconds
+    // Format a number with a leading zero if it's a single digit
+    const formatNumber = (number) => {
+        return number < 10 ? `0${number}` : `${number}`;
+    };
+    // Converting seconds into Days
+    const days = Math.floor(seconds / 86400);
+    // Converting seconds into Hours
+    const hours = Math.floor((seconds % 86400) / 3600);
+    // Converting seconds into Minutes
+    const minutes = Math.floor((seconds % 3600) / 60);
+    // Converting seconds into Seconds
+
 
     /* Initalizing counter interval on page load */
-
     useEffect(() => {
         const timer = setInterval(() => {
             if (seconds > 0) {
                 setSeconds(seconds - 1);
-                setAnimationClass((prevClass) => (prevClass === 'drop'? 'drop1' : 'drop'))
-                setAnimationClass1((prevClass) => (prevClass === 'below'? 'below1' : 'below'))
-                setResetAnimation(true)
             } else {
                 clearInterval(timer);
             }
         }, 1000);
-    
+
         return () => {
             clearInterval(timer);
         };
     }, [seconds]);
-    
-    const onANimationEnd = () => {
-    setResetAnimation(false)
-    }
-
 
     return (
         <div className='flex mt-24'>
-            <div className='flex flex-col items-center mx-4 relative'>
-                <div className="bg-timer relative rounded-md mx-2">
-                    <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
-                    <span className={`text-rose-400 text-8xl p-4 ${hours === 0 ? '' : ''}`}>{formatNumber(days)}</span>
-                </div>
-                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>DAYS</span>
-            </div>
-            <div className='flex flex-col items-center mx-4 relative'>
-                <div className="bg-timer relative rounded-md mx-2">
-                    <div className="absolute top-0 right-0 bottom-1/2 left-0 bg-[#252737] opacity-50"></div>
-                    <span className={`text-rose-400 text-8xl p-4 ${minutes === 0 ? '' : ''}`}>{formatNumber(hours)}</span>
-                </div>
-                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>HOURS</span>
-            </div>
-            <div className='flex flex-col items-center mx-4 relative'>
-                <div className={`bg-timer relative rounded-md mx-2`}>
-                    <div className={`flip-container ${remainingSeconds === 0 ? 'drop' : ''}`}>
-                        <span className={`text-rose-400 absolute text-8xl${remainingSeconds === 0 ? 'drop' : ''}`}>{formatNumber(minutes)}</span>
-                    </div>
-                    <span className={`text-rose-400 text-8xl p-4 ${remainingSeconds === 0 ? '' : ''}`}>{formatNumber(minutes)}</span>
-                </div>
-                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>MINUTES</span>
-            </div>
-            {/* Start : main working  */}
-            <div className='flex flex-col items-center mx-4 relative'>
+            {/* Start : Days  */}
+            <div className='flex flex-col items-center mx-1 md:mx-4 relative'>
                 <div className="bg-timer relative flex justify-center items-center overflow-hidden rounded-md mx-2">
-                    <div className={`flip-container ${animationClass} ${resetAnimation ? 'pause-animation' : ''}`}>
-                        <span className='text-rose-400 absolute text-8xl'>{formatNumber(remainingSeconds)}</span>
+                    <div className="overlay"></div>
+                    <div className={`flip-container ${hours === 0 ? 'drop1' : ''}`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(days)}</span>
                     </div>
-                    <div className={`flip-container-bottom ${animationClass1} ${resetAnimation ? 'pause-animation' : ''}`} onAnimationEnd={onANimationEnd}>
-                        <span className='text-rose-400 absolute text-8xl'>{formatNumber(remainingSeconds)}</span>
+                    <div className={`flip-container-bottom ${hours === 0 ? 'below1' : ''}`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(days)}</span>
                     </div>
-                    <span className='text-rose-400 absolute text-8xl'>{formatNumber(remainingSeconds)}</span>
-                    <span className='text-rose-400 text-8xl'>{formatNumber(remainingSeconds)}</span>
+                    <span className='text-rose-400 absolute text-timer'>{formatNumber(days)}</span>
+                    <span className='text-rose-400 text-timer'>{formatNumber(days)}</span>
                 </div>
-                <span className='text-gray-500 tracking-[5px] text-xs mt-10'>SECONDS</span>
+                <span className='text-gray-500 text-[8px] sm:text-sm tracking-[5px] mt-10'>DAYS</span>
             </div>
-            {/* End : main working  */}
+            {/* End : Days  */}
+
+            {/* Start : Hours  */}
+            <div className='flex flex-col items-center mx-1 md:mx-4 relative'>
+                <div className="bg-timer relative flex justify-center items-center overflow-hidden rounded-md mx-2">
+                    <div className="overlay"></div>
+                    <div className={`flip-container ${minutes === 0 ? 'drop1' : ''}`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(hours)}</span>
+                    </div>
+                    <div className={`flip-container-bottom ${minutes === 0 ? 'below1' : ''}`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(hours)}</span>
+                    </div>
+                    <span className='text-rose-400 absolute text-timer'>{formatNumber(hours)}</span>
+                    <span className='text-rose-400 text-timer'>{formatNumber(hours)}</span>
+                </div>
+                <span className='text-gray-500 text-[8px] sm:text-sm tracking-[5px] mt-10'>HOURS</span>
+            </div>
+            {/* End : Hours  */}
+
+            {/* Start : Minutes  */}
+            <div className='flex flex-col items-center mx-1 md:mx-4 relative'>
+                <div className="bg-timer relative flex justify-center items-center overflow-hidden rounded-md mx-2">
+                    <div className="overlay"></div>
+                    <div className={`flip-container ${remainingSeconds === 0 ? 'drop1' : ''}`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(minutes)}</span>
+                    </div>
+                    <div className={`flip-container-bottom ${remainingSeconds === 0 ? 'below1' : ''}`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(minutes)}</span>
+                    </div>
+                    <span className='text-rose-400 absolute text-timer'>{formatNumber(minutes)}</span>
+                    <span className='text-rose-400 text-timer'>{formatNumber(minutes)}</span>
+                </div>
+                <span className='text-gray-500 text-[8px] sm:text-sm tracking-[5px] mt-10'>MINUTES</span>
+            </div>
+            {/* End : Minutes  */}
+
+            {/* Start : Seconds  */}
+            <div className='flex flex-col items-center mx-1 md:mx-4 relative'>
+                <div className="bg-timer relative flex justify-center items-center overflow-hidden rounded-md mx-2">
+                    <div className="overlay"></div>
+                    <div className={`flip-container drop`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(remainingSeconds)}</span>
+                    </div>
+                    <div className={`flip-container-bottom below`}>
+                        <span className='text-rose-400 absolute text-timer'>{formatNumber(remainingSeconds)}</span>
+                    </div>
+                    <span className='text-rose-400 absolute text-timer'>{formatNumber(remainingSeconds)}</span>
+                    <span className='text-rose-400 text-timer'>{formatNumber(remainingSeconds)}</span>
+                </div>
+                <span className='text-gray-500 text-[8px] sm:text-sm tracking-[5px] mt-10'>SECONDS</span>
+            </div>
+            {/* End : Seconds  */}
         </div>
     );
 };
